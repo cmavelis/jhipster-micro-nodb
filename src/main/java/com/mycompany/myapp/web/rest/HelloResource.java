12a9fcdb0e236d018cdaad7b8c1d01cbfcf1233d
Cameron Avelis <cmavelis@gmail.com>
2019-07-11 09:54:30 -0400

    trying to return json from controller

--- src/main/java/com/mycompany/myapp/web/rest/HelloResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/HelloResource.java
@@ -2,10 +2,15 @@ package com.mycompany.myapp.web.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 /**
  * HelloResource controller
@@ -19,9 +24,8 @@ public class HelloResource {
     /**
     * GET greet
     */
-    @GetMapping("/greeting")
-    public String greet(@RequestParam(value = "name", defaultValue = "werld") String name) {
-        return "Hello " + name;
+    @GetMapping(name="/greeting", produces="application/json; charset=utf-8")
+    public Object greet(@RequestParam(value = "name", defaultValue = "werld") String name) {
+        return Collections.singletonMap("response", "Hello " + name);
     }
-
 }
